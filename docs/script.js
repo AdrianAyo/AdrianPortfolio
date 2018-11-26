23d67815a71979dc3d7b5065eda773a4600551d0
Adrian <donayo71@gmail.com>
2018-11-26 20:42:04 +0100

    added projects section, redesigned skillset

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-    $(".fa-html5").hover(function() {
-        $(".fa-html5").fadeIn("slow", function () {
-            $(".icons_card").addClass("icons_card_active");
-        });
+    $(".icons_element").hover(function() {
+            $(this).addClass("icons_card_active");
       }, function() {
-          $('.icons_card').removeClass('icons_card_active');
+          $(".icons_element").removeClass('icons_card_active');
       });
+
+    $(".projects_photo").hover(function () {
+        $(this).children(".projects_desc").addClass("desc_active");
+    }, function () {
+        $(".projects_desc").removeClass("desc_active");
+    });
 });
